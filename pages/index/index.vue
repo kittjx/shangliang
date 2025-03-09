381a4cd3c63a22d277f50402ca9eff89ec665da5
kittjx <kittjx@hotmail.com>
2025-03-09 23:33:15 +0800

    update

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,6 +2,14 @@
 	<view class="content">
 		<view class="font-active-color">文字</view>
 		<view class="iconfont icon-message-comments"></view>
+		<view class="iconfont icon-home"></view>
+		<view class="iconfont icon-home-fill"></view>
+		<view class="iconfont icon-all"></view>
+		<view class="iconfont icon-all-fill"></view>
+		<view class="iconfont icon-cart-full"></view>
+		<view class="iconfont icon-cart-full-fill"></view>
+		<view class="iconfont icon-customer"></view>
+		<view class="iconfont icon-customer-fill"></view>
 	</view>
 </template>
 
