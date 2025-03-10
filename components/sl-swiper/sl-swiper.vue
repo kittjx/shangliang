b644216bf97d066c213e897fc3bb9b36fd0faa18
kittjx <kittjx@hotmail.com>
2025-03-10 20:01:39 +0800

    update

---
+++ components/sl-swiper/sl-swiper.vue
@@ -0,0 +1,75 @@
+<template>
+  <view class="swiper-container">
+    <swiper 
+      class="swiper" 
+      autoplay="true" 
+      interval="3000" 
+      duration="1000" 
+      circular="true" 
+      indicator-dots="true">
+      
+      <swiper-item v-for="(item, index) in images" :key="index">
+        <view class="swiper-item">
+          <image :src="item" mode="aspectFit" class="swiper-pic" />
+        </view>
+      </swiper-item>
+
+    </swiper>
+  </view>
+</template>
+
+<script setup>
+
+const images = [
+  '/static/pic/banner1.jpg',
+  '/static/pic/banner2.jpg',
+  '/static/pic/banner3.jpg',
+  '/static/pic/banner4.jpg'
+];
+
+</script>
+
+<style lang="scss" scoped>
+  
+.swiper-container {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-item {
+  width: 100%;
+  height: 100%;
+  background-color: #f0f0f0;
+  color: #333;
+  font-size: 24px;
+  border-radius: 10px;
+}
+
+.uni-swiper-dot {
+  width: 8px;
+  height: 8px;
+  background-color: #999;
+  border-radius: 50%;
+  transition: background-color 0.3s;
+}
+ 
+.uni-swiper-dot-active {
+  background-color: #007aff;
+}
+
+.swiper-pic {
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+  
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,22 +1,13 @@
 <template>
-	<view class="content">
-		<view class="font-active-color">文字</view>
-		<view class="iconfont icon-message-comments"></view>
-		<view class="iconfont icon-home"></view>
-		<view class="iconfont icon-home-fill"></view>
-		<view class="iconfont icon-all"></view>
-		<view class="iconfont icon-all-fill"></view>
-		<view class="iconfont icon-cart-full"></view>
-		<view class="iconfont icon-cart-full-fill"></view>
-		<view class="iconfont icon-customer"></view>
-		<view class="iconfont icon-customer-fill"></view>
-	</view>
+  <view>
+    <sl-swiper></sl-swiper>
+  </view>
 </template>
 
-<script>
+<script setup>
 
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
