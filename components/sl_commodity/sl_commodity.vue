3787654f87a0bffba8ab5e9ab3024466410eb5df
kittjx <kittjx@hotmail.com>
2025-03-26 20:33:23 +0800

    update

---
+++ components/sl_commodity/sl_commodity.vue
@@ -0,0 +1,90 @@
+<template>
+  <view class="commodity-item">
+      <image class="commodity-pic" :src="commodity.pic" mode="aspectFill"></image>
+      <text class="commodity-text">{{ commodity.text }}</text>
+      <view class="price-container">
+        <text class="curpri">¥{{ commodity.curpri }}</text>
+        <text class="oldpri">¥{{ commodity.oldpri }}</text>
+      </view>
+      <text class="discount">{{ commodity.discount }}</text>
+    </view>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  commodity: {
+    type: Object,
+    required: true
+  }
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+.commodity-item {
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10upx; /* 可添加适当间距 */
+
+  .commodity-pic {
+    width: 100%;
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
+    border-radius: 8upx; /* 可选样式 */
+  }
+
+  .commodity-text {
+    padding: 6upx 10upx;
+    margin-top: 10upx;
+    font-size: 24upx;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-height: 1.5;
+    height: calc(2 * 1.5em);
+    width: 100%;
+    text-align: center;
+  }
+
+  .price-container {
+    width: 100%;
+    padding: 0 10upx;
+    margin-top: 8upx;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    .curpri {
+      color: red;
+      font-size: 28upx;
+    }
+    
+    .oldpri {
+      color: #999;
+      font-size: 24upx;
+      text-decoration: line-through;
+      margin-left: 10upx;
+    }
+  }
+
+  .discount {
+    color: white;
+    background-color: red;
+    border-radius: 4upx;
+    font-size: 24upx;
+    padding: 2upx 8upx;
+    margin-top: 8upx;
+    align-self: flex-start;
+    margin-left: 10upx;
+    align-self: center;
+  }
+}
+
+</style>
--- components/sl_commoditylist/sl_commoditylist.vue
+++ components/sl_commoditylist/sl_commoditylist.vue
@@ -1,16 +1,6 @@
 <template>
   <view class="commoditylist">
-    
-    <view class="commodity-item" v-for="(item, index) in commodities" :key="index">
-      <image class="commodity-pic" :src="item.pic" mode="aspectFill"></image>
-      <text class="commodity-text">{{item.text}}</text>
-      <view>
-        <text class="curpri">{{item.curpri}}</text>
-        <text class="oldpri">{{item.oldpri}}</text>
-      </view>
-      <text class="discount">{{item.discount}}</text>
-    </view>
-    
+    <sl_commodity class="commodity" v-for="(item, index) in commodities" :key="index" :commodity="item" />
   </view>
 </template>
 
@@ -29,48 +19,8 @@ const props = defineProps({
   flex-wrap: wrap;
 }
 
-.commodity-item {
-  width: 50%; /* 每个商品占 50% 宽度 */
-  box-sizing: border-box; /* 防止 padding 和 border 影响宽度 */
-  display: flex;
-  flex-direction: column; /* 垂直排列 */
-  align-items: center; /* 内容水平居中 */
+.commodity {
+  width: 50%;
 }
 
-.commodity-pic {
-  width: 100%;
-  aspect-ratio: 1 / 1; /* 宽高比为 1:1，保持正方形 */
-  object-fit: cover; /* 保持图片比例，裁剪多余部分 */
-}
-
-.commodity-text {
-  margin-top: 1upx;
-  font-size: 24upx;
-  display: -webkit-box; /* 使用弹性盒子布局 */
-  -webkit-box-orient: vertical; /* 垂直排列 */
-  -webkit-line-clamp: 2; /* 限制显示两行 */
-  overflow: hidden; /* 超出部分隐藏 */
-  text-overflow: ellipsis; /* 超出部分显示省略号 */
-  line-height: 1.5; /* 设置行高 */
-  height: calc(2 * 1.5em); /* 固定高度为两行的高度 */
-}
-
-.curpri {
-  color: red;
-  font-size: 28upx;
-}
-
-.oldpri {
-  color: #999;
-  font-size: 24upx;
-  text-decoration: line-through;
-  margin-left: 10upx;
-}
-
-.discount {
-  color: white;
-  background-color: red;
-  border-radius: 4upx;
-  font-size: 24upx;
-}
 </style>
